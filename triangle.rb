c3e2422332fdae3543f513c679305c19ddc24b16
Charles Reace <cwreace@yahoo.com>
2017-05-10 23:07:03 -0400

    more

--- triangle.rb
+++ triangle.rb
@@ -14,6 +14,9 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  if (a + b <= c) or (a + c <= b) or (c + b <= a)
+    raise TriangleError
+  end
   if a == b and b == c
     :equilateral
   elsif a == b or a == c or b == c
